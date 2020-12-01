353eedef5a4790a58041ea8f41f75658d9006d11
Jake Hansen <JakeMHandsome95@gmail.com>
2020-12-01 07:54:43 -0600

    Setup and Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var inputs = Array.ConvertAll(File.ReadAllLines("Input.txt"), x => int.Parse(x));
+
+         var part1 = (from a in inputs
+                      from b in inputs
+                      where a + b == 2020
+                      select (a * b)).First();
+         Console.WriteLine($"Part1: {part1}");
+
+         var part2 = (from a in inputs
+                      from b in inputs
+                      from c in inputs
+                      where a + b + c == 2020
+                      select (a * b * c)).First();
+         Console.WriteLine($"Part2: {part2}");
+      }
+   }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# AdventOfCode2020
+
+A place to store my work on the 2020 AdventOfCode https://adventofcode.com/2020
